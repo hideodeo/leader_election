9002545faef809bf1acd2ebab8be3ef14848c307
Hideo Kobayashi <pillows.pa.ko@gmail.com>
2017-04-14 15:06:37 +0900

    isCycle

--- src/entities/MyCycle.java
+++ src/entities/MyCycle.java
@@ -8,10 +8,19 @@ package entities;
 public class MyCycle {
     /** グラフ */
     private MyGraph<MyVertex, MyEdge> cycle;
-    
+
     /**
      * コンストラクタ
      */
     public MyCycle(){
+
+    }
+
+    public boolean isCycle(MyGraph<MyVertex, MyEdge> cycleIn){
+        for (MyVertex v: cycleIn.getVertices()){
+            if (cycleIn.inDegree(v) != 2)
+                return false;
+        }
+        return true;
     }
 }
