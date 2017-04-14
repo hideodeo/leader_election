c36c5e3ba908e18f27c0d8ba1f09527f5f7aeee9
Hideo Kobayashi <pillows.pa.ko@gmail.com>
2017-04-14 11:24:54 +0900

    conflict解消

--- src/entities/MyVertex.java
+++ src/entities/MyVertex.java
@@ -8,17 +8,6 @@ import org.apache.commons.collections15.Factory;
  * 頂点を表すクラス
  */
 public class MyVertex {
-<<<<<<< HEAD
-    boolean DFSLabel=false;
-
-    public void setDFSLabel(boolean DFSLabel) {
-        this.DFSLabel = DFSLabel;
-    }
-
-    public boolean getDFSLabel() {
-        return this.DFSLabel;
-=======
-
     /**
      * ファクトリを返すクラスメソッド
      * GraphGeneratorで使う
@@ -31,6 +20,5 @@ public class MyVertex {
                 return new MyVertex();
             }
         };
->>>>>>> fcfa753ff1de835c7c6d8da2b34bbdae5a94298f
     }
 }
--- src/generators/TreeGenerator.java
+++ src/generators/TreeGenerator.java
@@ -1,22 +1,5 @@
 package generators;
 
-<<<<<<< HEAD
-import entities.MyGraph;
-
-/**
- * Created by Hideo on 2017/04/11.
- *
- * TreeGenerator
- *
- * ツリーを作成するクラスのためのインターフェース
- */
-public interface TreeGenerator {
-    /**
-     * ツリーを作成して返すメソッド
-     * @param graphIn
-     */
-    void create(MyGraph graphIn);
-=======
 import entities.MyEdge;
 import entities.MyGraph;
 import entities.MyVertex;
@@ -32,5 +15,4 @@ public interface TreeGenerator {
      * @return 木
      */
     MyGraph<MyVertex, MyEdge> create();
->>>>>>> fcfa753ff1de835c7c6d8da2b34bbdae5a94298f
 }
