08582f1b8a578775b89d05fb3b1d9a693f3a20ca
Hideo Kobayashi <pillows.pa.ko@gmail.com>
2017-04-15 23:12:00 +0900

    initial commit

---
+++ src/algorithms/SharedEdge.java
@@ -0,0 +1,7 @@
+package algorithms;
+
+/**
+ * Created by Hideo on 2017/04/15.
+ */
+public class SharedEdge {
+}
--- src/algorithms/SharedVertex.java
+++ src/algorithms/SharedVertex.java
@@ -2,6 +2,8 @@ package algorithms;
 
 /**
  * SharedVertex
+ *
+ * # of shared vertexesに基いてリーダを選ぶクラス
  */
 public class SharedVertex {
 }
