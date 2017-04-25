4b685c748775c74729bb18b717c34cfd65d5794c
Hideo Kobayashi <pillows.pa.ko@gmail.com>
2017-04-25 11:37:02 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import algorithms.RandomAlgorithm;
+import algorithms.SharedVertexAlgorithm;
 import entities.MyCycle;
 import entities.MyEdge;
 import entities.MyGraph;
 import entities.MyVertex;
-import generators.BFSTreeGenerator;
-import generators.FundamentalCyclesGenerator;
-import generators.NWSGraphGenerator;
+import generators.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Hideo on 17/04/11.
@@ -28,28 +29,48 @@ public class Main {
     }
 
     public static void excute(String graphNameIn, String treeNameIn, int vertexNumIn, String algorithmNameIn) {
-        /** initialize graph, tree, cycle list and leader map*/
-        MyGraph<MyVertex, MyEdge> graph;
+        /** initialization */
+        MyGraph<MyVertex, MyEdge> graph = new MyGraph<MyVertex, MyEdge>();
         List<MyVertex> vertexList;
-        MyGraph<MyVertex, MyEdge> tree;
+        MyGraph<MyVertex, MyEdge> tree = new MyGraph<MyVertex, MyEdge>();
         List<MyCycle> cycles;
         Map<MyCycle, MyVertex> leadersMap;
 
-        if (graphNameIn == "NWS"){
-
+        /** create graph */
+        if (graphNameIn == "NWS") {
             NWSGraphGenerator NWSGenerator = new NWSGraphGenerator(vertexNumIn);
             graph = NWSGenerator.create();
-            vertexList = new ArrayList<MyVertex>(graph.getVertices());
+        }
+        else if (graphNameIn == "BA"){
+            BAGraphGenerator BAGenerator = new BAGraphGenerator(vertexNumIn);
+            graph = BAGenerator.create();
+        }
 
+        /** get vertexes from graph */
+        vertexList = new ArrayList<MyVertex>(graph.getVertices());
+
+        /** create tree */
+        if (treeNameIn == "BFS") {
             BFSTreeGenerator BFSTreeGenerator = new BFSTreeGenerator(graph, vertexList.get(0));
             tree = BFSTreeGenerator.create();
+        }
+        else if (treeNameIn == "DFS"){
+            DFSTreeGenerator DFSTreeGenerator = new DFSTreeGenerator(graph, vertexList.get(0));
+            tree = DFSTreeGenerator.create();
+        }
 
-            FundamentalCyclesGenerator cyclesGenerator = new FundamentalCyclesGenerator(graph, tree);
-            cycles = cyclesGenerator.create();
+        /** create cycles */
+        FundamentalCyclesGenerator cyclesGenerator = new FundamentalCyclesGenerator(graph, tree);
+        cycles = cyclesGenerator.create();
 
-            RandomAlgorithm raAl = new RandomAlgorithm(graph, cycles);
-            leadersMap = raAl.solve();
-
+        /** elect leaders */
+        if (algorithmNameIn == "SharedVertex") {
+            SharedVertexAlgorithm al = new SharedVertexAlgorithm(graph, cycles);
+            leadersMap = al.solve();
+        }
+        else if (algorithmNameIn == "Random") {
+            RandomAlgorithm al = new RandomAlgorithm(graph, cycles);
+            leadersMap = al.solve();
         }
     }
 }
