0e56de87a2c2f78bdb518c2a9a9492ebfb6665f0
Hideo Kobayashi <pillows.pa.ko@gmail.com>
2017-04-27 13:47:44 +0900

    modified ResultsWriter

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import algorithms.Algorithm;
-import algorithms.RandomAlgorithm;
-import algorithms.SharedVertexAlgorithm;
-
+import IO.ResultsWriter;
+import algorithms.*;
 import calculators.EvaluationFunctions;
-
 import entities.MyCycle;
 import entities.MyEdge;
 import entities.MyGraph;
@@ -11,19 +8,20 @@ import entities.MyVertex;
 import generators.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * Created by Hideo on 17/04/11.
+ * Main
  *
+ * This class sets conditions and conducts simulations.
  */
 public class Main {
     public static void main(String[] args) {
         /** parameters for simulation */
         int simulationTimes = 10;
-        int initialNumOfVertex = 20, maxNumOfVertex = 100, incrementalNumOfVertex = 20;
+        int initialNumOfVertex = 20, maxNumOfVertex = 60, incrementalNumOfVertex = 20;
+        /** prepare a list for the num of vertexes */
         List<Integer> numOfVertexList = getVertexList(initialNumOfVertex, maxNumOfVertex, incrementalNumOfVertex);
 
         /** execute simulations specifying settings*/
@@ -39,18 +37,28 @@ public class Main {
      * @param algorithmNameIn
      */
     private static void execute(String graphNameIn, String treeNameIn, List<Integer> vertexNumIn, String algorithmNameIn, int simulationTimesIn) {
-        /** initialize map for data collection */
-        Map<Integer, Double> dataMap = new HashMap<Integer, Double>();
-        for (int num: vertexNumIn){
-            dataMap.put(num, 0.0);
+        /** prepare lists for data collection */
+        List<List<Double>> dataLists = new ArrayList<List<Double>>();
+        List<Double> numOfVertex = new ArrayList<Double>();
+        List<Double> valueOfObjectiveFunction = new ArrayList<Double>();
+
+        /** collect data of the number of vertexes */
+        for (int num: vertexNumIn) {
+            numOfVertex.add((double) num);
+        }
+        dataLists.add(numOfVertex);
+
+        /** collect data of objective function by iterating according to the number of vertexes */
+        /** initialize the list for data collection */
+        for (int i=0; i < vertexNumIn.size(); i++){
+            valueOfObjectiveFunction.add(0.0);
         }
 
-        /** iterate according to the number of vertexes */
-        for (int num: vertexNumIn) {
-            /** execute simulations */
-            for (int i=0; i < simulationTimesIn; i++) {
+        /** execute simulations for leader election */
+        for (int i=0; i < vertexNumIn.size(); i++) {
+            for (int j=0; j < simulationTimesIn; j++) {
                 /** create graph */
-                MyGraph<MyVertex, MyEdge> graph = getGraphGenerator(graphNameIn, num).create();
+                MyGraph<MyVertex, MyEdge> graph = getGraphGenerator(graphNameIn, vertexNumIn.get(i)).create();
 
                 /** get vertexes from graph */
                 List<MyVertex> vertexList = new ArrayList<MyVertex>(graph.getVertices());
@@ -65,11 +73,18 @@ public class Main {
                 /** elect leaders */
                 Map<MyCycle, MyVertex> leadersMap = getAlgorithm(algorithmNameIn, graph, cycles).solve();
 
-                /** save data into map */
-                dataMap.put(num, dataMap.get(num) + EvaluationFunctions.objectiveFunction(graph, cycles, leadersMap));
+                /** save data into list */
+                valueOfObjectiveFunction.add(i, valueOfObjectiveFunction.get(i) + EvaluationFunctions.objectiveFunction(graph, cycles, leadersMap));
             }
+            /** calculate average values following the number of simulation */
+            valueOfObjectiveFunction.add(i, valueOfObjectiveFunction.get(i) / simulationTimesIn);
         }
+        dataLists.add(valueOfObjectiveFunction);
+
         /** output results into csv file */
+        ResultsWriter writer = new ResultsWriter();
+        String filePath = writer.getFullPath(graphNameIn, treeNameIn, algorithmNameIn);
+        writer.write(dataLists, filePath, "# of vertexes", "value of objective function");
     }
 
     /**
@@ -89,6 +104,7 @@ public class Main {
 
     /**
      * return tree generator according to tree name
+     *
      * @param treeNameIn
      * @param rootIn
      * @return tree generator
@@ -103,6 +119,7 @@ public class Main {
 
     /**
      * return algorithm instance according to algorithm name
+     *
      * @param algorithmNameIn
      * @param graphIn
      * @param cyclesIn
@@ -111,11 +128,23 @@ public class Main {
     private static Algorithm getAlgorithm (String algorithmNameIn, MyGraph<MyVertex, MyEdge> graphIn, List<MyCycle> cyclesIn){
         if (algorithmNameIn == "SharedVertex")
             return new SharedVertexAlgorithm(graphIn, cyclesIn);
+        else if (algorithmNameIn == "Closeness")
+            return new ClosenessAlgorithm(graphIn, cyclesIn);
         else if (algorithmNameIn == "Random")
-             return new RandomAlgorithm(graphIn, cyclesIn);
+            return new RandomAlgorithm(graphIn, cyclesIn);
+        else if (algorithmNameIn == "OPT")
+            return new OPTAlgorithm(graphIn, cyclesIn);
         return null;
     }
 
+    /**
+     * return a list representing the number of vertexes
+     *
+     * @param initialNumOfVertex
+     * @param maxNumOfVertex
+     * @param incrementalNumOfVertex
+     * @return
+     */
     private static List<Integer> getVertexList(int initialNumOfVertex, int maxNumOfVertex, int incrementalNumOfVertex){
         List<Integer> result = new ArrayList<Integer>();
         for (int i=initialNumOfVertex; i<=maxNumOfVertex; i+=incrementalNumOfVertex) {
